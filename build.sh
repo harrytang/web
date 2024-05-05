074ccc7651c933104599f1d06fbb3f17d013233b
Harry Tang <harry@maxspell.com>
2024-05-05 09:58:07 +0300

    chore: updates docker file and build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Source the environment variables from .env file
+if [ -f .env.build.local ]; then
+    export $(cat .env.build.local | grep -v '^#' | xargs)
+fi
+
+# Docker build command with all the ARG values
+docker build \
+    --network=host \
+    --build-arg NEXT_PUBLIC_STRAPI_API_URL=${NEXT_PUBLIC_STRAPI_API_URL} \
+    --build-arg NEXT_PUBLIC_STORAGE_HOST=${NEXT_PUBLIC_STORAGE_HOST} \
+    --build-arg NEXT_PUBLIC_SITE_NAME=${NEXT_PUBLIC_SITE_NAME} \
+    --build-arg NEXT_PUBLIC_SITE_URL=${NEXT_PUBLIC_SITE_URL} \
+    --build-arg NEXT_PUBLIC_CA_PUB=${NEXT_PUBLIC_CA_PUB} \
+    --build-arg NEXT_PUBLIC_PAGE_SIZE=${NEXT_PUBLIC_PAGE_SIZE} \
+    --build-arg NEXT_PUBLIC_CSE_ID=${NEXT_PUBLIC_CSE_ID} \
+    --build-arg NEXT_PUBLIC_HOME_PAGE_SIZE=${NEXT_PUBLIC_HOME_PAGE_SIZE} \
+    --build-arg STRAPI_API_URL=${NEXT_PUBLIC_STRAPI_API_URL} \
+    -t ghcr.io/harrytang/web:dev .
