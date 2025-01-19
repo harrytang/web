fa352948aca0f41d03b5d3ef15c77e81a9ef3ba7
Harry Tang <harry@maxspell.com>
2025-01-19 21:52:07 +0200

    chore: update to react 19

--- src/hooks/usePrevious.ts
+++ src/hooks/usePrevious.ts
@@ -1,7 +1,12 @@
 import { useEffect, useRef } from 'react'
 
-const usePrevious = <T>(value: T) => {
-  let ref = useRef<T>()
+/**
+ * Hook to retrieve the previous value of a prop or state.
+ * @param value - The current value.
+ * @returns The previous value, or `undefined` if it doesn't exist.
+ */
+const usePrevious = <T>(value: T): T | undefined => {
+  const ref = useRef<T | undefined>(undefined)
 
   useEffect(() => {
     ref.current = value
