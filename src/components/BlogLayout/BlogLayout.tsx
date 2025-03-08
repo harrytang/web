d3eb6506c6a687212bb3cc4f6d53815f684f9487
Harry Tang <harry@maxspell.com>
2025-03-08 11:29:41 +0200

    chore: disable lazy load for article main image

--- src/components/BlogLayout/BlogLayout.tsx
+++ src/components/BlogLayout/BlogLayout.tsx
@@ -35,7 +35,7 @@ const BlogLayout: React.FC<BlogLayoutProps> = ({ blog }) => {
               type="button"
               onClick={() => router.back()}
               aria-label="Go back to articles"
-              className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
+              className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md ring-1 shadow-zinc-800/5 ring-zinc-900/5 transition lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
             >
               <ArrowLeftIcon className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
             </button>
@@ -74,6 +74,8 @@ const BlogLayout: React.FC<BlogLayoutProps> = ({ blog }) => {
                   alt={blog.attributes.seo.metaImage.data.attributes.caption}
                   width={blog.attributes.seo.metaImage.data.attributes.width}
                   height={blog.attributes.seo.metaImage.data.attributes.height}
+                  priority={true}
+                  loading="eager"
                 />
                 <figcaption className="mt-4 flex justify-center gap-x-2 text-sm leading-6 text-gray-500">
                   <InformationCircleIcon
