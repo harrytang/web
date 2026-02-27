55a9dd8a499d165d14629e6f0f1bbe5132ae3dfd
Harry Tang <harry@maxspell.com>
2026-02-27 14:54:40 +0200

    refactor(NavLink): Handle external links correctly

--- src/components/Footer/NavLink.tsx
+++ src/components/Footer/NavLink.tsx
@@ -6,11 +6,20 @@ type NavLinkProps = {
 }
 
 const NavLink: React.FC<NavLinkProps> = ({ href, children }) => {
+  const isExternal = /^(https?:|mailto:|tel:|\/\/)/.test(href)
+
+  const className = 'transition hover:text-amber-600 dark:hover:text-amber-600'
+
+  if (isExternal) {
+    return (
+      <a href={href} className={className} target="_blank" rel="noopener noreferrer">
+        {children}
+      </a>
+    )
+  }
+
   return (
-    <Link
-      href={href}
-      className="transition hover:text-amber-600 dark:hover:text-amber-600"
-    >
+    <Link href={href} className={className}>
       {children}
     </Link>
   )
