0c7274ea8acc258ea5470218ee81f5895d43d670
Harry Tang <harry@maxspell.com>
2026-05-16 18:58:26 +0000

    chore: update deps

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_STORAGE_HOST,
-        port: '',
-      },
-    ],
-  },
-  rewrites: async () => [
-    {
-      source: '/articles',
-      destination: '/articles/1', // Serve content from /articles/1 when visiting /articles
-    },
-    {
-      source: '/:slug',
-      destination: '/page/:slug',
-    },
-  ],
-  redirects: async () => [
-    {
-      source: '/page/:slug',
-      destination: '/:slug',
-      permanent: true,
-    },
-    {
-      source: '/articles/1',
-      destination: '/articles',
-      permanent: true,
-    },
-  ],
-  output: 'standalone',
-}
+	pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+	allowedDevOrigins: ["local.harrytang.xyz"],
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: process.env.NEXT_PUBLIC_STORAGE_HOST,
+				port: "",
+			},
+		],
+	},
+	rewrites: async () => [
+		{
+			source: "/articles",
+			destination: "/articles/1", // Serve content from /articles/1 when visiting /articles
+		},
+		{
+			source: "/:slug",
+			destination: "/page/:slug",
+		},
+	],
+	redirects: async () => [
+		{
+			source: "/page/:slug",
+			destination: "/:slug",
+			permanent: true,
+		},
+		{
+			source: "/articles/1",
+			destination: "/articles",
+			permanent: true,
+		},
+	],
+	output: "standalone",
+};
 
-export default nextConfig
+export default nextConfig;
